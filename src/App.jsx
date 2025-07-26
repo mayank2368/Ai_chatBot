3780f573d32a6d7e09de290342bb54dd701631da
Mayank-Admin <mayank-admin@Mayanks-Air.local>
2025-07-26 21:30:25 +0200

    Add the header, body, footer styles  and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,44 @@ import ChatbotIcon from "./components/ChatbotIcon";
 const App = () => {
   return (
     <div className="container">
-      <div className="chatbot-pop">
+      <div className="chatbot-popup">
+        {/* Chatbot header */}
         <div className="chat-header">
           <div className="header-info">
             <ChatbotIcon />
             <h2 className="logo_text">Chatbot</h2>
           </div>
+          <button className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
+        </div>
+        {/* Chatbot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there!!!👋😉
+              <br /> How can I help you today?
+            </p>
+          </div>
+
+          <div className="message user-message">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        {/* Chatbot footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input
+              type="text"
+              placeholder="Go on lad...ask me!"
+              className="message-input"
+              required
+            />
+            <button className="material-symbols-rounded">arrow_upward</button>
+          </form>
         </div>
       </div>
     </div>
